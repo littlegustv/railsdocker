cffae2c433c08459e9c9d9fffbcfbcd197443d73
Benjamin Heller <echo29x@gmail.com>
2018-04-03 14:37:41 -0400

    pre-docker commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+  
+  before_action :set_model, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    render "new"
+  end
+
+  def update
+    @article.update(articles_params)
+    redirect_to @article
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    @article.save
+    redirect_to @article
+  end
+
+  def show
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  def set_model
+    @article = Article.find(params[:id])
+  end
+end
